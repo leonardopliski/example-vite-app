7a2ffabd930d4a815ea2867c6ff5d1ff762ae11f
leonardopliski <leonardopliski@gmail.com>
2021-07-13 00:12:24 -0300

    feat: add simple test application

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import classNames from "classnames";
+import "./styles/index.scss";
+
+const ThemeSwitcher = () => {
+  const [isDarkThemeEnabled, setIsDarkThemeEnabled] = React.useState(false);
+
+  const classes = classNames({
+    app: true,
+    dark: isDarkThemeEnabled,
+    light: !isDarkThemeEnabled,
+  });
+
+  const handleCheckboxInputClick = () => {
+    setIsDarkThemeEnabled(!isDarkThemeEnabled);
+  };
+
+  return (
+    <div className={classes}>
+      <h1>Vite Test App!</h1>
+
+      <label className="toggle">
+        <input
+          className="toggle-checkbox"
+          type="checkbox"
+          value={isDarkThemeEnabled}
+          onClick={handleCheckboxInputClick}
+        />
+        <div className="toggle-switch"></div>
+        <span className="toggle-label">
+          {isDarkThemeEnabled ? "Dark Theme" : "Light Theme"}
+        </span>
+      </label>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeSwitcher />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
